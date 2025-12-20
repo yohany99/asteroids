48c20bf727c88e81ec094d153250ec8c769e308c
yohany99 <yohany99@berkeley.edu>
2025-12-19 16:39:06 -0800

    add logger.py, constants.py, set up gameplay loop in main

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        delta = clock.tick(60)
+        dt = delta / 1000
+
+if __name__ == "__main__":
+    main()
