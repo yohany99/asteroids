9f2f040fd24de2ce0f91b9a55d6291c868262e69
yohany99 <yohany99@berkeley.edu>
2025-12-22 14:43:43 -0800

    implement player and circleshape classes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 
@@ -14,6 +15,7 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -21,6 +23,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         delta = clock.tick(60)
         dt = delta / 1000
